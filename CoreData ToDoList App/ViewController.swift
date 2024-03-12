6cd00a2bbf384b7d866181ac161adfc493e8ca48
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2024-03-12 17:16:51 +0100

    Initial Commit

---
+++ CoreData ToDoList App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData ToDoList App
+//
+//  Created by Macbook Air 2017 on 12. 3. 2024..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
